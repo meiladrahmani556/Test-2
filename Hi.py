4c1ab416df74a0a7567a0a885c717fca2ddec2d7
MeiladRah556 <meilad.rahmani123@gmail.com>
2025-12-02 00:03:00 +0000

    Enhance user interaction with movie and song inputs
    Added user prompts for movie and song preferences, and calculations for ticket prices.

--- Hi.py
+++ Hi.py
@@ -1,3 +1,32 @@
 print("Hello you?)
+movie = input("What's your favourite movie? ")
+print("The movie name is", movie)
+times_watched = input("How many times did you watch that movie? ")
+print("You watched", movie, times_watched, "times.")
 
-      
+song = input("What's your favourite song? ")
+print("the name of the song is", song) 
+times_heard = input("how often do you listen to the song per week? ") 
+print("you heard", song, times_heard, "times. ") 
+
+a = 23
+b = 24 
+result = a + b 
+print(result) 
+a = 50
+b = 2
+result = a / b
+print(result) 
+
+price = input("Enter the price of a movie ticket: ") 
+price = float(price) 
+total = price * 5 
+print("total cost for 5 movie tickets is", total) 
+price = input("how much is the price of 1 moive ticekt")
+price = input("how many ticket do you want") 
+
+price = input("Enter the price of one movie ticket: ") 
+price = float(price) 
+ticket_count = int(input("How many tickets do you want?"))
+result = price * ticket_count 
+print("Total cost is", result)
